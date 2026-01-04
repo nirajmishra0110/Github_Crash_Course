bd11408e61d0717c7ea8d4713890ae556eb772f9
Niraj Mishra <niraj.mishra0110@gmail.com>
2026-01-04 06:47:57 -0500

    added console message

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const dob="01/01/2000"
 function getuserInfo(){
     return [age, _name, dob]
 }
-module.exports=getuserInfo
-//exporting the function to be used in other files  
+
+console.log(getuserInfo())
